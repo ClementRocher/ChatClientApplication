c016a52693f8da85975301651f6deb92b0cb1822
Clément Rocher <clementrocher71@gmail.com>
2018-03-26 15:12:07 +0200

    Socket Client

--- ChatClientApplication/Program.cs
+++ ChatClientApplication/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Net.Sockets;
@@ -21,42 +22,18 @@ namespace ChatClientApplication
         {
             try 
             {
-                // Create a TcpClient.
-                // Note, for this client to work you need to have a TcpServer 
-                // connected to the same address as specified by the server, port
-                // combination.
                 Int32 port = 4200;
-                
-                TcpClient client = new TcpClient("127.0.0.1", port);
+                IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000);
+                Socket client = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+                client.Connect(ipep);
+               
     
                 // Translate the passed message into ASCII and store it as a Byte array.
                 Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);         
 
                 // Get a client stream for reading and writing.
-                //  Stream stream = client.GetStream();
-    
-                NetworkStream stream = client.GetStream();
-
-                // Send the message to the connected TcpServer. 
-                stream.Write(data, 0, data.Length);
-
-                Console.WriteLine("Sent: {0}", message);         
-
-                // Receive the TcpServer.response.
-    
-                // Buffer to store the response bytes.
-                data = new Byte[256];
-
-                // String to store the response ASCII representation.
-                String responseData = String.Empty;
-
-                // Read the first batch of the TcpServer response bytes.
-                Int32 bytes = stream.Read(data, 0, data.Length);
-                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-                Console.WriteLine("Received: {0}", responseData);         
-
-                // Close everything.
-                client.Close();         
+                //byte[] data = new byte[1024];
+                int sent = client.Send(data);
             } 
             catch (ArgumentNullException e) 
             {
