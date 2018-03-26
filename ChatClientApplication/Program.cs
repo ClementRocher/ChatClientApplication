f8bb8ada737daf9d05f16124bb8b69e2cde696f5
Jean-Baptiste Crespo <jbc.waagh@gmail.com>
2018-03-26 16:16:41 +0200

    solution stream

--- ChatClientApplication/Program.cs
+++ ChatClientApplication/Program.cs
@@ -21,42 +21,21 @@ namespace ChatClientApplication
         {
             try 
             {
-                // Create a TcpClient.
-                // Note, for this client to work you need to have a TcpServer 
-                // connected to the same address as specified by the server, port
-                // combination.
-                Int32 port = 4200;
-                
-                TcpClient client = new TcpClient("127.0.0.1", port);
-    
-                // Translate the passed message into ASCII and store it as a Byte array.
-                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);         
-
-                // Get a client stream for reading and writing.
-                //  Stream stream = client.GetStream();
-    
+                // Initialization (Create and connect)
+                TcpClient client = new TcpClient("127.0.0.1", 4200);
                 NetworkStream stream = client.GetStream();
-
-                // Send the message to the connected TcpServer. 
+                // Send operation
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
                 stream.Write(data, 0, data.Length);
-
-                Console.WriteLine("Sent: {0}", message);         
-
-                // Receive the TcpServer.response.
-    
-                // Buffer to store the response bytes.
+                // Receive operation
                 data = new Byte[256];
-
-                // String to store the response ASCII representation.
                 String responseData = String.Empty;
-
-                // Read the first batch of the TcpServer response bytes.
                 Int32 bytes = stream.Read(data, 0, data.Length);
                 responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-                Console.WriteLine("Received: {0}", responseData);         
-
-                // Close everything.
-                client.Close();         
+                Console.WriteLine("Received : " + responseData);
+                // It’s done !
+                stream.Close();
+                client.Close();      
             } 
             catch (ArgumentNullException e) 
             {
@@ -68,7 +47,7 @@ namespace ChatClientApplication
             }
     
             Console.WriteLine("\n Press Enter to continue...");
-            Console.Read();
+            //Console.Read();
         }
 
     }
