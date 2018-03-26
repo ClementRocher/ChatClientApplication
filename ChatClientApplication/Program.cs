642a24409a65121c1597a477f971e59e3464b7b8
Jean-Baptiste Crespo <jbc.waagh@gmail.com>
2018-03-26 17:30:21 +0200

    tg

--- ChatClientApplication/Program.cs
+++ ChatClientApplication/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace ChatClientApplication
 {
@@ -11,14 +12,11 @@ namespace ChatClientApplication
         static void Main(string[] args)
         {
             String message;
-            do
-            {
-                message = Console.ReadLine();
-                Connect(message);
-            } while (message != "/exit");
+            new Program().Connect();
         }
-        
-        static void Connect(String message) 
+
+
+        private void Connect() 
         {
             try 
             {
@@ -26,19 +24,23 @@ namespace ChatClientApplication
                 // Initialization (Create and connect)
                 TcpClient client = new TcpClient("127.0.0.1", 4200);
                 NetworkStream stream = client.GetStream();
-                // Send operation
-                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
-                stream.Write(data, 0, data.Length);
-                // Receive operation
-                data = new Byte[256];
-                String responseData = String.Empty;
-                Int32 bytes = stream.Read(data, 0, data.Length);
-                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-                Console.WriteLine("Received : " + responseData);
+                new Thread((o) => { Reading(stream,client); });
+                String message = "";
+                
+                while(client.Available>0 || message.Equals("/exit"))
+                {
+                    // Send operation
+                    
+                    message = Console.ReadLine();
+                    Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+                    stream.Write(data, 0, data.Length);
+                   
+                }
+                
+               
                 // It’s done !
                 stream.Close();
                 client.Close();      
-
             } 
             catch (ArgumentNullException e) 
             {
@@ -49,8 +51,18 @@ namespace ChatClientApplication
                 Console.WriteLine("SocketException: {0}", e);
             }
     
-            Console.WriteLine("\n Press Enter to continue...");
-            //Console.Read();
+        }
+
+        private void Reading(NetworkStream stream,TcpClient client)
+        {
+            while(client.Available>0)
+            {
+                Byte[] data = new Byte[256];
+                String responseData = String.Empty;
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received : " + responseData);
+            }
         }
 
     }
